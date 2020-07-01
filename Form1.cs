c6a6a611cdcff01d8658210a2c5390a8dcd3fef7
Julia <tyto.alba.1999@gmail.com>
2020-07-02 00:58:30 +0300

    Feature implementation

--- Form1.cs
+++ Form1.cs
@@ -11,18 +11,28 @@ using System.IO;
 
 namespace Transport_Company
 {
-    
+
     public partial class Form1 : Form
     {
-        public int[,] Price = new int[20, 20];
+        public Deixtra Algo;
+        public Node[] Nodes = new Node[20];
+
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 20; i++)
+            {
+                Nodes[i] = new Node();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Nodes[Convert.ToInt32(textBox2.Text) - 1].CurrentValue = 0;
+            Algo = new Deixtra(Nodes);
+            Algo.Run(Convert.ToInt32(textBox2.Text) - 1);
+            label1.Text = "Стоимость: " + Convert.ToString(Algo.nodes[Convert.ToInt32(textBox3.Text) - 1].CurrentValue);
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -33,27 +43,27 @@ namespace Transport_Company
         private void button2_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFile = new OpenFileDialog();
-            if (openFile.ShowDialog() ==  DialogResult.OK)
+            if (openFile.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = openFile.FileName;
                 try
                 {
                     String[] Mas = File.ReadAllLines(textBox1.Text);
                     string[] TempStrings;
-
                     for (int i = 0; i < Mas.Length; i++)
                     {
                         TempStrings = Mas[i].Split(';');
-                        Price[Convert.ToInt32(TempStrings[0])-1, Convert.ToInt32(TempStrings[1])-1] = Convert.ToInt32(TempStrings[2]);
-                        Price[Convert.ToInt32(TempStrings[1]) - 1, Convert.ToInt32(TempStrings[0]) - 1] = Convert.ToInt32(TempStrings[2]);
+                        Nodes[Convert.ToInt32(TempStrings[0]) - 1].Roads.Add(new int[2] { Convert.ToInt32(TempStrings[1]) - 1, Convert.ToInt32(TempStrings[2]) });
+                        Nodes[Convert.ToInt32(TempStrings[1]) - 1].Roads.Add(new int[2] { Convert.ToInt32(TempStrings[0]) - 1, Convert.ToInt32(TempStrings[2]) });
                     }
                     label2.Text = "Статус: база загружена";
                 }
-                catch {
-                   label2.Text = "Статус: ошибка при чтении файла!";
+                catch
+                {
+                    label2.Text = "Статус: ошибка при чтении файла!";
                 }
 
-            } 
+            }
         }
     }
 }
