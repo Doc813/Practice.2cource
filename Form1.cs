bcfef5b25c67a5b4e44424f63d6eba0d4e61360a
Julia <tyto.alba.1999@gmail.com>
2020-07-01 03:09:47 +0300

    Feature Final Version

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Transport_Company
 {
+    
     public partial class Form1 : Form
     {
+        public int[,] Price = new int[20, 20];
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +36,22 @@ namespace Transport_Company
             if (openFile.ShowDialog() ==  DialogResult.OK)
             {
                 textBox1.Text = openFile.FileName;
+                try
+                {
+                    String[] Mas = File.ReadAllLines(textBox1.Text);
+                    string[] TempStrings;
+
+                    for (int i = 0; i < Mas.Length; i++)
+                    {
+                        TempStrings = Mas[i].Split(';');
+                        Price[Convert.ToInt32(TempStrings[0])-1, Convert.ToInt32(TempStrings[1])-1] = Convert.ToInt32(TempStrings[2]);
+                    }
+                    label2.Text = "Статус: база загружена";
+                }
+                catch {
+                   label2.Text = "Статус: ошибка при чтении файла!";
+                }
+
             } 
         }
     }
